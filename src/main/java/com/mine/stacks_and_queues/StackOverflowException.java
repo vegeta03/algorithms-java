0ac992294ec37cc635eba423b87981dc8c35f3c1
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2020-05-08 17:38:29 +0530

    stack exception classes.

---
+++ src/main/java/com/mine/stacks_and_queues/StackOverflowException.java
@@ -0,0 +1,8 @@
+package com.mine.stacks_and_queues;
+
+public class StackOverflowException extends Exception {
+
+    public StackOverflowException() {
+        super("Stack overflow exception.");
+    }
+}
---
+++ src/main/java/com/mine/stacks_and_queues/StackUnderflowException.java
@@ -0,0 +1,7 @@
+package com.mine.stacks_and_queues;
+
+public class StackUnderflowException extends Exception {
+    StackUnderflowException() {
+        super("Stack underflow exception.");
+    }
+}
