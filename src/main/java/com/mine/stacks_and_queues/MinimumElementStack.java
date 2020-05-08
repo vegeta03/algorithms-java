7c02e70eea4c1469af268ce01482ebdd65e8692c
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2020-05-09 01:15:22 +0530

    MinimumElementStack

---
+++ src/main/java/com/mine/stacks_and_queues/MinimumElementStack.java
@@ -0,0 +1,27 @@
+package com.mine.stacks_and_queues;
+
+public class MinimumElementStack {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minimumStack = new Stack<>();
+
+    public void push(int data) throws StackOverflowException, StackUnderflowException {
+        int min = data;
+
+        if (!minimumStack.isEmpty()) {
+            if (min > minimumStack.peek()) {
+                min = minimumStack.peek();
+            }
+        }
+        stack.push(data);
+        minimumStack.push(min);
+    }
+
+    public int pop() throws StackUnderflowException {
+        minimumStack.pop();
+        return stack.pop();
+    }
+
+    public int getMinimum() throws StackUnderflowException {
+        return minimumStack.peek();
+    }
+}
